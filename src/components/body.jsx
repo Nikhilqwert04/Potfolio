2194e75986a283911bd3c8954c51447f47ebf044
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-07-27 20:50:45 +0530

    Work

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeroSection from './body/herosection'
 import Work from "./body/work"
+import Expertise from "./body/expertise"
+import Carrer from "./body/carrer"
 
 const body = () => {
   return (
     <div>
       <HeroSection/>
       <Work/>
+      <Expertise/>
+      <Carrer/>
     </div>
   )
 }
---
+++ src/components/body/carrer.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const carrer = () => {
+  return (
+    <div>
+          <section class="py-12 px-6">
+        <div class="h-auto w-auto">
+            <div class="text-[#cfff45] text-2xl h-fit w-fit my-7 font-['Brotesk']" id="Career">--Career Path</div>
+            <div class="text-white text-3xl h-fit w-auto font-bold">Building & Learning</div>
+            <div class="w-auto h-auto  mt-10 flex justify-center items-center flex-col relative ">
+                <div class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 top-80 left-1 animate-[float_7s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] 
+                    shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/diamond.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 left-10 top-10 animate-[float_3s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/web-development.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 left-50 top-80 animate-[float_3s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/linux.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 left-20 top-50 animate-[float_3s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/technology.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 left-50 top-30 animate-[float_3s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/creativity.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 right-50 top-55 animate-[float_3s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/web-development.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 right-5 top-16 animate-[float_4s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/lightning.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 right-60 top-10 animate-[float_4s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/linux.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 right-10 top-80 animate-[float_4s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/creativity.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 right-30 top-30 animate-[float_4s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/gemini.png" alt="diamond" class=""></img>
+                </div>
+                <div
+                    class="h-14 w-15 bg-[#1a1a1a] border border-[#9b979772] rounded-2xl flex justify-center items-center absolute p-3 ml-2 right-65 top-90 animate-[float_4s_ease-in-out_infinite] max-[530px]:h-[10vw] max-[530px]:w-[10vw] max-[500px]:p-[2vw] shadow-[0_0_25px_8px_rgba(125,158,34,0.2)]">
+                    <img src="/java.png" alt="diamond" class=""></img>
+                </div>
+
+
+                <div
+                    class=" bg-[#1a1a1a] flex flex-col h-auto max-w-300  p-10 rounded-4xl m-2.5  hover:border-2 hover:border-[#cfff45] hover:shadow-[inset_0_0_10px_rgba(207,255,69,1)] transition-all duration-100 overflow-hidden group/carrier z-5 shadow-[0_0_25px_8px_rgba(125,158,34,0.3)] hover:-translate-y-2">
+                    <div class="text-2xl max-[613px]:text-lg overflow-clip text-white flex flex-col">
+
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Passionate about learning <span
+                                class="group-hover/carrier:text-[#7d9e22] transition-all duration-400 ">modern
+                                technologies</span> that improve productivity and user experience</div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Like creating smooth,
+                            interactive, and visually clean user <span
+                                class=" group-hover/carrier:text-[#a6d12f] transition-all duration-400 ">interfaces</span>
+                        </div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Focus on both <span
+                                class=" group-hover/carrier:text-[#cfff45] transition-all">frontend design</span> and
+                            <span class=" group-hover/carrier:text-[#cfff45] transition-all duration-400 ">backend
+                                functionality</span>
+                        </div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Build projects driven by
+                            curiosity and practical <span
+                                class=" group-hover/carrier:text-[#dcff6c]  transition-all duration-400 ">problem-solving</span>
+                        </div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span>Continuously learning through
+                            projects, open-source, and experimentation</div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Interested in system <span
+                                class=" group-hover/carrier:text-[#e8ff9e]  transition-all duration-400 ">customization</span>
+                            and optimizing digital workflows</div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Enjoy transforming ideas into
+                            polished and user-friendly digital experiences</div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Constantly refine and improve
+                            projects with creativity and <span
+                                class=" group-hover/carrier:text-[#f4ffd1] transition-all duration-400 ">innovation</span>
+                        </div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Love combining functionality with
+                            aesthetics to create engaging experiences</div>
+                        <div><span class="group-hover/carrier:text-[#cfff45]">✦</span> Enjoy exploring <span
+                                class=" group-hover/carrier:text-[#fbfff0]  transition-all duration-400 ">Linux</span>
+                            customization and experimenting with developer workflows</div>
+                    </div>
+                    <div
+                        class="h-2 w-2 text-7xl ml-auto mr-65 font-['Brotesk'] text-[#494545] group-hover/carrier:text-[#cfff45] transition-all duration-200">
+                        2026</div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+  )
+}
+
+export default carrer
