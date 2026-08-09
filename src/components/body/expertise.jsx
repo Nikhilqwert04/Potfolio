41eaeddf0934173254fc490d6304fdbea15a9e51
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-08-09 22:13:38 +0530

    refactor: reformat expertise component and standardize code style across files

--- src/components/body/expertise.jsx
+++ src/components/body/expertise.jsx
@@ -1,65 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const expertise = () => {
   return (
     <div>
-          <section class="py-12 px-6">
+      <section class="py-12 px-6">
         <div class="h-auto   w-auto">
-            <div class="text-[#cfff45] text-2xl h-fit w-fit my-7 font-['Brotesk']" id="Expertise">-- EXPERTISE</div>
-            <div class="text-white text-3xl h-fit w-auto font-bold">Tech Stack & Tools + Future Learnings</div>
-            <div class=" px-1.5 py-3 w-auto flex gap-7 mt-10 flex-wrap justify-center items-center max-[789px]:gap-5">
-                <div
-                    class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
-                    <img src="/html.png" alt="HTML" class="bg-cover"></img>
-                </div>
-                <div
-                    class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
-                    <img src="/text.png" alt="CSS" class="bg-cover"></img>
-                </div>
-                <div
-                    class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
-                    <img src="/tailwind.png" alt="Tailwind" class="bg-cover"></img>
-                </div>
-                <div
-                    class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
-                    <img src="/github.png" alt="Github" class="bg-cover"></img>
-                </div>
-                <div
-                    class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
-                    <img src="/git.png" alt="Github" class="bg-cover"></img>
-                </div>
-
-                <div
-                    class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
-                    <img src="/java-script.png" alt="Github" class="bg-cover"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/nextjs.png" alt="CSS" class="bg-cover blur-xs"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/linux.png" alt="CSS" class="bg-cover"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/monogo.png" alt="CSS" class="bg-cover"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/socketio.png" alt="CSS" class="bg-cover blur-xs"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/gemini.png" alt="CSS" class="bg-cover blur-xs"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/java.png" alt="CSS" class="bg-cover blur-xs"></img>
-                </div>
-                <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
-                    <img src="/python.png" alt="CSS" class="bg-cover blur-xs"></img>
-                </div>
-
+          <div
+            class="text-[#cfff45] text-2xl h-fit w-fit my-7 font-['Brotesk']"
+            id="Expertise"
+          >
+            -- EXPERTISE
+          </div>
+          <div class="text-white text-3xl h-fit w-auto font-bold">
+            Tech Stack & Tools + Future Learnings
+          </div>
+          <div class=" px-1.5 py-3 w-auto flex gap-7 mt-10 flex-wrap justify-center items-center max-[789px]:gap-5">
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/html.png" alt="HTML" class="bg-cover"></img>
             </div>
-        </div>
-    </section>
-    </div>
-  )
-}
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/text.png" alt="CSS" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/tailwind.png" alt="Tailwind" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/github.png" alt="Github" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/git.png" alt="Github" class="bg-cover"></img>
+            </div>
 
-export default expertise
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/java-script.png" alt="Github" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border grayscale-40 max-[1355px]:grayscale-0 hover:grayscale-0 active:grayscale-0 hover:border-[#cfff45] active:border-[#cfff45] hover:shadow-[2px_2px_2px_2px_#cfff45] active:shadow-[2px_2px_2px_2px_#cfff45] hover:scale-110 active:scale-95 transition-all duration-300 cursor-pointer">
+              <img src="/gemini.png" alt="Github" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
+              <img src="/nextjs.png" alt="CSS" class="bg-cover blur-xs"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
+              <img src="/linux.png" alt="CSS" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
+              <img src="/monogo.png" alt="CSS" class="bg-cover"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
+              <img src="/socketio.png" alt="CSS" class="bg-cover blur-xs"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
+              <img src="/java.png" alt="CSS" class="bg-cover blur-xs"></img>
+            </div>
+            <div class="w-20 bg-[#1a1a1a] p-2 rounded-2xl border-[#676363] border cursor-pointer">
+              <img src="/python.png" alt="CSS" class="bg-cover blur-xs"></img>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default expertise;
