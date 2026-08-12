f25303c9aa1a0a83c08ba7219af82052e13a8714
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-08-13 02:39:08 +0530

    feat: install axios for API integration and remove unused Career component from App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
 import Body from "./components/body"
-import Carrer from './components/body/carrer'
 import Contact from './components/body/contact'
 import SubNavbar from './components/body/SubNavbar'
 import CustomCursor from './components/customCursor'
@@ -13,7 +12,6 @@ const App = () => {
       <CustomCursor />
       <Navbar />
       <Body/>
-      <Carrer/>
       <Contact/>
       <SubNavbar/>
       <Footer />
